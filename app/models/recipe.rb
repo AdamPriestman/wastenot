529fb309fd9a400a12eab369b67b50c4e76ee0d2
Pooja Gurung <pooja_grg07@hotmail.com>
2023-11-29 11:37:46 +0000

    container-show created. working on the recipe-info-card now

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,5 @@ class Recipe < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :posts
   has_many :ingredients, through: :recipe_ingredients
+  has_many_attached :photos
 end
