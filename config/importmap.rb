b09f3ff74d9abd398712f3cb3ae67668f0fceea7
Kata Nagy <nagykata98@gmail.com>
2023-11-30 11:29:15 +0000

    Navbar changes

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "js-autocomplete", to: "https://ga.jspm.io/npm:js-autocomplete@1.0.4/auto-complete.min.js"
