2ab676da4f3f5d0dd7661a34811245df1f15da04
Abshai Missongo <abshaim@yahoo.com>
2023-11-30 14:55:46 +0000

    validations and merging

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
+  validates :recipe_id, uniqueness: { scope: :bookmarks, message: "this recipe is already bookmarked" }
 end
