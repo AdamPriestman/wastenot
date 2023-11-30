9c72cb99da059b906f5cb683eaacd512b9d13f9e
Abshai Missongo <abshaim@yahoo.com>
2023-11-30 15:08:40 +0000

    working on validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
-  validates :recipe_id, uniqueness: { scope: :bookmarks, message: "this recipe is already bookmarked" }
+  # validates :recipe_id, uniqueness: { scope: :bookmarks, message: "this recipe is already bookmarked" }
 end
