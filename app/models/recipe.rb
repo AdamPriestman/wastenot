676c8d727b37c1c8b2062c7d9a7b8ab19dfc4dc5
Pooja Gurung <pooja_grg07@hotmail.com>
2023-12-01 11:49:56 +0000

    working on the community feed still

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,13 @@ class Recipe < ApplicationRecord
   has_many :posts
   has_many :ingredients, through: :recipe_ingredients
   validates :title, :description, presence: true
+
+  # def compute_average_rating
+  #   posts = self.posts
+  #   ratings_array = []
+  #   posts.each do |post|
+  #     raitings_array << post.ratings
+  #   end
+  #   ratings_array.all / ratings_array.length
+  # end
 end
