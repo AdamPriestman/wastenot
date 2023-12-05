13c515f9e93b18e650afdebdfed6a13016211092
Kata Nagy <nagykata98@gmail.com>
2023-12-05 09:17:52 +0000

    Some styling

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -2,10 +2,12 @@ const header = document.querySelector(".header");
 let lastScroll = 0;
 window.addEventListener("scroll", () => {
   let currentScroll = window.scrollY;
+  console.log(header.style.top);
   if (currentScroll < lastScroll) {
     header.style.top = "0";
   } else {
-    header.style.top = "-50px";
+    header.style.top = "-70px";
   }
   lastScroll = currentScroll;
+  console.log(lastScroll);
 })
