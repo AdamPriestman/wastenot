08822e591c973ac6a8e0e72f3c6e3c7567f9e443
AdamPriestman <adampriestman@gmail.com>
2023-12-05 11:26:51 +0000

    index page finished (backend)

---
+++ app/javascript/controllers/navbar_icon_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar-icon"
+export default class extends Controller {
+  static targets = ["home", "feed", "search", "bookmark"]
+  connect() {
+    const link = window.location.href.split("/")
+    const iconValue = (link[link.length - 1])
+    if (iconValue === ""){
+      this.homeTarget.classList.add("active")
+    } else if (iconValue === "posts"){
+      this.feedTarget.classList.add("active")
+    } else if (iconValue === "search" ){
+      this.searchTarget.classList.add("active")
+    } else if (iconValue === "bookmarks"){
+      this.bookmarkTarget.classList.add("active")
+    } else {
+      this.searchTarget.classList.add("active")
+    }
+  }
+}
