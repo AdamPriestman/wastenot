95416c376470a3d26652dd4d2dcbec7b7a17f532
Abshai Missongo <abshaim@yahoo.com>
2023-12-06 14:25:36 +0000

    working on changing bookmark icon colour

--- app/javascript/controllers/add_bookmark_controller.js
+++ app/javascript/controllers/add_bookmark_controller.js
@@ -2,13 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add-bookmark"
 export default class extends Controller {
-  static targets = ["bookmark"]
+  static targets = ["bookmark", "switch", "recipe"]
   static values = {
     id: String,
     recipe: String
   }
   connect() {
-    // console.log(this.idTarget.innerText)
+    const link = window.location.href.split("/")
+    const recipes = this.recipeTargets
+    console.log(recipes)
+    const recipeId = this.recipeValue
+    console.log(recipeValue)
+    const iconValue = (link[link.length - 1])
+    console.log(iconValue)
+    if (iconValue === `${recipeId}`){
+      this.switchTarget.classList.add("green")
+      this.switchTarget.classList.remove("white")
+    } else {
+      this.switchTarget.classList.remove("green")
+      this.switchTarget.classList.add("white")
+    }
   }
 
   create_bookmark(event){
