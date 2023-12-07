ecdfc85d9c3ca7eee194e62c2496879b6d53ec5e
AdamPriestman <adampriestman@gmail.com>
2023-12-07 17:42:13 +0000

    cta bigger

--- app/javascript/controllers/navbar_icon_controller.js
+++ app/javascript/controllers/navbar_icon_controller.js
@@ -12,6 +12,9 @@ export default class extends Controller {
       this.searchTarget.classList.add("active")
     } else if (iconValue === "bookmarks"){
       this.bookmarkTarget.classList.add("active")
+    } else if (iconValue === "") {
+      console.log("no icons green")
+      this.searchTarget.classList.remove("active")
     } else {
       this.searchTarget.classList.add("active")
     }
